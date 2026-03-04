7b59c948976633cca68bc7fdd078ee0971ac22b9
ninnoke <nkepu20@freeuni.edu.ge>
2026-03-04 14:17:53 +0400

    create fixed aucio player with interactions.structured layout using CSS grid and flexbox. added hover effects for buttons. add JS for play/pause events. player is fixed at bottom of viewport

--- script.js
+++ script.js
@@ -5,3 +5,41 @@ function selectIcon(el, group) {
 
   el.classList.add("selected");
 }
+const sections = document.querySelectorAll(".slider-section");
+
+sections.forEach((section) => {
+  const cataloge = section.querySelector(".cataloge");
+  const arrows = section.querySelectorAll(".list-title-line ul li img");
+
+  if (!cataloge || arrows.length < 2) return;
+
+  const leftArrow = arrows[0];
+  const rightArrow = arrows[1];
+
+  const card = cataloge.querySelector("li");
+  const gap = 10;
+  const scrollAmount = card.offsetWidth + gap;
+
+  rightArrow.addEventListener("click", () => {
+    cataloge.scrollLeft += scrollAmount;
+  });
+
+  leftArrow.addEventListener("click", () => {
+    cataloge.scrollLeft -= scrollAmount;
+  });
+});
+////////
+//////
+//////
+const audio = document.getElementById("audio");
+const playBtn = document.getElementById("playBtn");
+const player = document.querySelector(".player");
+playBtn.addEventListener("click", () => {
+  if (audio.paused) {
+    audio.play();
+    player.classList.add("active");
+  } else {
+    audio.pause();
+    player.classList.remove("active");
+  }
+});
